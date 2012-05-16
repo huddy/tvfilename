96e47d2090c1cc840509f55b2deb0d692ee84d57
Billy Howard <billy@osg.me>
2012-05-16 14:47:36 +0100

    adding new handler to handle EPXX matches.

--- handlers/format5.php
+++ handlers/format5.php
@@ -4,18 +4,18 @@ namespace tvfilename\handlers;
 
 require_once 'handlerInterface.php';
 
-class format4 implements handlerInterface {
+class format5 implements handlerInterface {
 
     public $episode;
     public $season;
     public $string;
-    private $_regex = '/ep(\d\d)/i'; //EPXX case insenstive. We assume here there's only one season.;
+    private $_regex = '/\D+([1-9])(\d\d)\D+/i'; //matches stargate.atlantis.409.dsr.xvid.notv.avi - we assume season is first single digit.;
 
     public function match($string) {
         $matches = array();
         if (preg_match($this->_regex, $string, $matches) > 0) {
-            $this->season = (int) 1;
-            $this->episode = (int) $matches[1];
+            $this->season = (int) $matches[1];
+            $this->episode = (int) $matches[2];
             $this->string = $string;
             return true;
         }
